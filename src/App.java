0921c3b490b6f18756867eab6bf677495b669469
Miguel Angel Villegas <cromigue8@gmail.com>
2026-01-06 12:01:24 -0500

    Se creo el menu imprimible

--- src/App.java
+++ src/App.java
@@ -1,5 +1,22 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        imprimir();
+    }
+
+    static void imprimir(){
+        var opciones = """
+                ---Sistema de Registro de Estudiantes---
+                
+                1. Registrar Estudiante
+                2. Mostrar Estudiantes
+                3. Calcular Promedio de Notas
+                4. Mostrar resumen completo del estudiante
+                5. Limpiar datos del estudiante actual
+                0. Salir
+                """;
+
+        System.out.println(opciones);
+        System.out.println("Seleccione una opcion: ");
     }
 }
